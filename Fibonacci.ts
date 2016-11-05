24d5ef4393de257e2a13e3004eaa193e294a4769
Vadim Rybak <vrybak@vertmarkets.com>
2016-11-05 11:09:43 -0400

    added reactive extensions version of fibonacci

--- Fibonacci.ts
+++ Fibonacci.ts
@@ -1,5 +1,5 @@
 import * as _ from "lodash";
-
+import { Observable, Observer } from "rxjs";
 
 export class Fibonacci {
 
@@ -11,10 +11,11 @@ export class Fibonacci {
 
     public sequence(size: number): number[] {
         return _
-            .range(0,size)
-            .map( i => this.fn(i));
+            .range(0, size)
+            .map(i => this.fn(i));
 
     }
+
 
     private fn(n: number): number {
         if (n == 0)
@@ -22,5 +23,19 @@ export class Fibonacci {
         if (n <= 2)
             return 1;
         return this.fn(n - 1) + this.fn(n - 2);
+    }
+
+    public observableSequence(n:number): Observable<number> {
+        return Observable.create((observer : Observer<number>) => {
+            let fn1 = 0;
+            let fn2 = 0;
+            for(let iter = 0; iter < n; iter++){
+                let current = fn2;
+                fn2 = fn1 == 0 ? 1 : fn1;
+                fn1 = (fn1 == 0 ? 1 : fn1) + current;
+                observer.next(current);
+            }
+            observer.complete();
+        })
     }
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import { Fibonacci } from "./Fibonacci";
+let colors = require("colors");
 
-if (process.argv.length !== 3){
+if (process.argv.length !== 3) {
     console.error("Usage: node index.js 10");
     process.exit();
 }
 let fib = new Fibonacci();
 let n = parseInt(process.argv[2]);
+
+
+console.log("Example running traditional Fibonacci sequence (recursion)")
 let sequence = fib.sequence(n);
-console.log(`Sequence to ${n} is \r\n [${sequence}]`);
+console.log(`Sequence to '${n}' is: `,colors.green(sequence));
+console.log("\r\n\r\n");
+console.log("Example running reactive/obserable Fibonacci sequence (rxjs)");
+let sequence2 :number[] = [];
+fib
+    .observableSequence(n)
+    .subscribe((n) => { 
+        sequence2.push(n);
+    }, 
+    (error) => {}, 
+    () => {
+        console.log("the sequence is: ", colors.rainbow(sequence2.join(",")));
+    } );
